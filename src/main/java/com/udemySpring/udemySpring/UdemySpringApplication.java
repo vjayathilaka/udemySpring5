c7fff445e1bc9ed9dcd4f81faa9852488393ba68
vijitha <vijithakn@gmail.com>
2018-06-10 09:11:25 +0530

    initial commit

---
+++ src/main/java/com/udemySpring/udemySpring/UdemySpringApplication.java
@@ -0,0 +1,12 @@
+package com.udemySpring.udemySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/udemySpring/udemySpring/UdemySpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.udemySpring.udemySpring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemySpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
